85380d5a2a8246a1e300baa1f25bac7adc3b49ac
xivk <ben@itinero.tech>
2018-03-28 11:26:27 +0200

    Added CLI to convert binary to xml format.

---
+++ src/OsmSharp.IO.Binary.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using OsmSharp.Streams;
+
+namespace OsmSharp.IO.Binary.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sourceStream = File.OpenRead(args[0]))
+            using (var targetStream = File.Open(args[1], FileMode.Create))
+            {
+                var sourceBinary = new BinaryOsmStreamSource(sourceStream);
+                var targetXml = new XmlOsmStreamTarget(targetStream);
+                targetXml.RegisterSource(sourceBinary);
+                targetXml.Pull();
+            }
+        }
+    }
+}
