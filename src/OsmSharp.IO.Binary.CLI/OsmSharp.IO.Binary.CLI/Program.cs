2b282b90262a0384770a18cad7ea6774f94b3db5
xivk <ben@itinero.tech>
2018-05-18 14:59:05 +0100

    Added a simple project to convert binary files to OSM-XML.

---
+++ src/OsmSharp.IO.Binary.CLI/OsmSharp.IO.Binary.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace OsmSharp.IO.Binary.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var inputStream = File.OpenRead(args[0]))
+            using (var outputStream = File.Open(args[1], FileMode.Create))
+            {
+                var osmInputStream = new OsmSharp.Streams.BinaryOsmStreamSource(inputStream);
+                var osmOutputStream = new OsmSharp.Streams.XmlOsmStreamTarget(outputStream);
+                osmOutputStream.RegisterSource(osmInputStream);
+                osmOutputStream.Pull();
+                osmOutputStream.Flush();
+            }
+        }
+    }
+}
