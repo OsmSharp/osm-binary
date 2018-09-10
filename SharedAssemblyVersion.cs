049563cbc1a51c14f7625171816280566be58390
Ben Abelshausen <ben@itinero.tech>
2018-09-10 22:23:33 +0100

    Update SharedAssemblyVersion.cs

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -19,5 +19,5 @@
 using System.Reflection;
 
 // update the version # here.
-[assembly: AssemblyInformationalVersion("0.0.12-alpha")]
-[assembly: AssemblyVersion("0.0.12")] 
+[assembly: AssemblyInformationalVersion("0.0.13-alpha")]
+[assembly: AssemblyVersion("0.0.13")] 
