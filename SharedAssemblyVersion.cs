c3ec4f76e9c9a5103228bf396330fa42db8dcdb0
xivk <ben@itinero.tech>
2019-01-09 12:47:40 +0000

    Updated functional tests with a compressed stream case.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -19,5 +19,5 @@
 using System.Reflection;
 
 // update the version # here.
-[assembly: AssemblyInformationalVersion("0.0.15-alpha")]
-[assembly: AssemblyVersion("0.0.15")] 
+[assembly: AssemblyInformationalVersion("0.0.16-alpha")]
+[assembly: AssemblyVersion("0.0.16")] 
--- test/OsmSharp.IO.Binary.Test.Functional/Program.cs
+++ test/OsmSharp.IO.Binary.Test.Functional/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.IO.Compression;
+using Serilog;
 
 namespace OsmSharp.IO.Binary.Test.Functional
 {
@@ -7,60 +9,105 @@ namespace OsmSharp.IO.Binary.Test.Functional
         public static void Main(string[] args)
         {
             // enable logging.
-            OsmSharp.Logging.Logger.LogAction = (o, level, message, parameters) =>
+            OsmSharp.Logging.Logger.LogAction = (origin, level, message, parameters) =>
             {
-                System.Console.WriteLine(string.Format("[{0}] {1} - {2}", o, level, message));
+                var formattedMessage = $"{origin} - {message}";
+                switch (level)
+                {
+                    case "critical":
+                        Log.Fatal(formattedMessage);
+                        break;
+                    case "error":
+                        Log.Error(formattedMessage);
+                        break;
+                    case "warning":
+                        Log.Warning(formattedMessage);
+                        break; 
+                    case "verbose":
+                        Log.Verbose(formattedMessage);
+                        break; 
+                    case "information":
+                        Log.Information(formattedMessage);
+                        break; 
+                    default:
+                        Log.Debug(formattedMessage);
+                        break;
+                }
             };
 
-            // test reading/writing standard OSM-data.
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine("logs", "log-.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // download test data.
             Download.ToFile("http://files.itinero.tech/data/OSM/planet/europe/luxembourg-latest.osm.pbf", "test.osm.pbf").Wait();
 
+            // test read/writing an existing OSM file.
+            Log.Information("Testing reading/writing via OSM binary format...");
             using (var sourceStream = File.OpenRead("test.osm.pbf"))
-            using (var targetStream = File.Open("test.osm.bin", FileMode.Create))
+            using (var targetStream = File.Open("test1.osm.bin", FileMode.Create))
             {
                 var source = new OsmSharp.Streams.PBFOsmStreamSource(sourceStream);
-                var progress = new OsmSharp.Streams.Filters.OsmStreamFilterProgress();
-                progress.RegisterSource(source);
 
                 var target = new OsmSharp.Streams.BinaryOsmStreamTarget(targetStream);
-                target.RegisterSource(progress);
+                target.RegisterSource(source);
                 target.Pull();
             }
-
-            using (var sourceStream = File.OpenRead("test.osm.bin"))
+            using (var sourceStream = File.OpenRead("test1.osm.bin"))
             using (var targetStream = File.Open("test2.osm.pbf", FileMode.Create))
             {
                 var source = new OsmSharp.Streams.BinaryOsmStreamSource(sourceStream);
-                var progress = new OsmSharp.Streams.Filters.OsmStreamFilterProgress();
-                progress.RegisterSource(source);
 
                 var target = new OsmSharp.Streams.PBFOsmStreamTarget(targetStream);
-                target.RegisterSource(progress);
+                target.RegisterSource(source);
+                target.Pull();
+            }
+            
+            // test read/writing an existing OSM file via a compressed stream.
+            Log.Information("Testing reading/writing via OSM binary format using a compressed stream...");
+            using (var sourceStream = File.OpenRead("test.osm.pbf"))
+            using (var targetStream = File.Open("test2.osm.bin.zip", FileMode.Create))
+            using (var targetStreamCompressed = new System.IO.Compression.DeflateStream(targetStream, CompressionLevel.Fastest))
+            {
+                var source = new OsmSharp.Streams.PBFOsmStreamSource(sourceStream);
+
+                var target = new OsmSharp.Streams.BinaryOsmStreamTarget(targetStreamCompressed);
+                target.RegisterSource(source);
+                target.Pull();
+            }
+
+            using (var sourceStream = File.OpenRead("test2.osm.bin.zip"))
+            using (var sourceStreamCompressed = new System.IO.Compression.DeflateStream(sourceStream, CompressionMode.Decompress))
+            using (var targetStream = File.Open("test2.osm.pbf", FileMode.Create))
+            {
+                var source = new OsmSharp.Streams.BinaryOsmStreamSource(sourceStreamCompressed);
+
+                var target = new OsmSharp.Streams.PBFOsmStreamTarget(targetStream);
+                target.RegisterSource(source);
                 target.Pull();
             }
 
             // test reading/writing edited OSM-data.
+            Log.Information("Testing reading/writing via OSM binary format of edited incomplete OSM data...");
             using (var sourceStream = File.OpenRead("./test-data/data.osm"))
-            using (var targetStream = File.Open("test.osm.bin", FileMode.Create))
+            using (var targetStream = File.Open("test3.osm.bin", FileMode.Create))
             {
                 var source = new OsmSharp.Streams.XmlOsmStreamSource(sourceStream);
-                var progress = new OsmSharp.Streams.Filters.OsmStreamFilterProgress();
-                progress.RegisterSource(source);
 
                 var target = new OsmSharp.Streams.BinaryOsmStreamTarget(targetStream);
-                target.RegisterSource(progress);
+                target.RegisterSource(source);
                 target.Pull();
             }
 
-            using (var sourceStream = File.OpenRead("test.osm.bin"))
+            using (var sourceStream = File.OpenRead("test3.osm.bin"))
             using (var targetStream = File.Open("test2.osm.pbf", FileMode.Create))
             {
                 var source = new OsmSharp.Streams.BinaryOsmStreamSource(sourceStream);
-                var progress = new OsmSharp.Streams.Filters.OsmStreamFilterProgress();
-                progress.RegisterSource(source);
 
                 var target = new OsmSharp.Streams.PBFOsmStreamTarget(targetStream);
-                target.RegisterSource(progress);
+                target.RegisterSource(source);
                 target.Pull();
             }
         }
