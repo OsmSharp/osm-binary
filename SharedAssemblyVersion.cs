b53ea1b2b20a4184d6463d24e3de342fd66bb009
xivk <ben@itinero.tech>
2020-03-26 15:30:13 +0000

    Updated all projects to latest netcore.

--- SharedAssemblyVersion.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-// update the version # here.
-[assembly: AssemblyInformationalVersion("0.0.18-alpha")]
-[assembly: AssemblyVersion("0.0.18")] 
--- build.bat
+++
@@ -1,3 +0,0 @@
-dotnet restore
-dotnet build -c release
-dotnet test .\test\OsmSharp.IO.Binary.Test\OsmSharp.IO.Binary.Test.csproj
